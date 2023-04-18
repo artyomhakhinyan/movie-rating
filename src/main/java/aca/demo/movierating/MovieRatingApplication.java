c533728b5ec389416ac9b056abfe84a64edd7587
Artyom Hakhinyan <artyom.artyom.01@gmail.com>
2023-04-18 13:20:53 +0400

    HW11 Added

--- src/main/java/aca/demo/movierating/MovieRatingApplication.java
+++ src/main/java/aca/demo/movierating/MovieRatingApplication.java
@@ -18,8 +18,8 @@ public class MovieRatingApplication {
 		movieService.create(new CreateMovie("Horrible Bosses",Genre.COMEDY));
 		movieService.create(new CreateMovie(" American Beauty",Genre.DRAMA));
 
-		log.info("Drama movies are{}",movieService.search(Genre.DRAMA).toString());
-		log.info("Romace movies are{}",movieService.search(Genre.ROMANCE).toString());
+		log.debug("Drama movies are{}",movieService.search(Genre.DRAMA).toString());
+		log.debug("Romace movies are{}",movieService.search(Genre.ROMANCE).toString());
 		try{movieService.create(new CreateMovie("Forest Gumb",Genre.COMEDY));}
 		catch(IllegalArgumentException e){
 			log.info("the movie already exists");
