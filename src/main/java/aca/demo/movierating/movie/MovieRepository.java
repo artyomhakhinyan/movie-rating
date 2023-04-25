32cef955708c87c0a8f0f653c0598ed45ab81f5e
Artyom Hakhinyan <artyom.artyom.01@gmail.com>
2023-04-25 22:50:32 +0400

    Changed MovieRepository class

--- src/main/java/aca/demo/movierating/movie/MovieRepository.java
+++ src/main/java/aca/demo/movierating/movie/MovieRepository.java
@@ -3,6 +3,7 @@ package aca.demo.movierating.movie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -12,18 +13,37 @@ import java.util.stream.Collectors;
 @Component
 public class MovieRepository {
     private List<Movie> movies = new ArrayList<>();
-    public Optional<Movie> findByTitle(String title) {
-        log.debug("Starts findByTitle method with title{}",title);
-        return movies.stream().filter(movie->movie.getTitle().equals(title)).findAny();
+
+    public Optional<Movie> findById(Long id) {
+        log.debug("Starts findById method with id -{}",id);
+        return movies.stream().filter(movie->movie.getTitle().equals(id)).findAny();
     }
-    public List<Movie> findByGenre(Genre genre) {
-        log.debug("Starts MovieRepository.findByGenre() method with genre{}",genre);
-        return movies.stream().filter(movie->movie.getGenre().equals(genre)).collect(Collectors.toList());
-    }
-    public void save(CreateMovie createMovie) {
-        log.debug("Starts MovieRepository.save() method with movie{}",createMovie.toString());
-        Movie movie=new Movie(createMovie);
+
+
+    public void persist(Movie movie) {
+        log.debug("Starts persist method with movie -{}",movie);
         movies.add(movie);
+    }
+
+    void delete(Movie movie) {
+        log.debug("Starts delete method with movie -{}",movie);
+     Optional<Movie> movieOptional=movies.stream().filter(movie1->movie1.getId().equals(movie.getId())).findAny();
+     if(movieOptional.isPresent())
+        movies.remove(movie);
+    }
+
+
+    List<Movie> search(Genre genre, String title, LocalDate releasedBefore, LocalDate releasedAfter) {
+            log.debug("Starts search method with genre-{},title-{},released before-{},released after-{}",
+                    title,genre,releasedBefore,releasedAfter);
+          return movies.stream().filter(movie ->
+                   ( title==null ||movie.getTitle().equals(title))&&
+                   (genre==null || movie.getGenre().equals(genre))&&
+                   (releasedBefore==null ||movie.getReleasedAt().compareTo(releasedBefore)>=0)&&
+                   ( releasedAfter==null ||movie.getReleasedAt().compareTo(releasedBefore)<=0) ).
+                  collect(Collectors.toUnmodifiableList());
+
+
     }
 
 }
