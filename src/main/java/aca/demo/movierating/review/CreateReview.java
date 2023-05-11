822452805ad909da0255e44a3272fa76a7d2c11a
Artyom Hakhinyan <artyom.artyom.01@gmail.com>
2023-05-11 12:51:03 +0400

    Added UpdateReview class

--- src/main/java/aca/demo/movierating/review/CreateReview.java
+++ src/main/java/aca/demo/movierating/review/CreateReview.java
@@ -1,9 +1,13 @@
 package aca.demo.movierating.review;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 import java.time.Instant;
 @Value
+@Builder
+@Jacksonized
 public class CreateReview {
     Long id;
     Long movieId;
--- src/main/java/aca/demo/movierating/review/UpdateReview.java
+++ src/main/java/aca/demo/movierating/review/UpdateReview.java
@@ -1,9 +1,13 @@
 package aca.demo.movierating.review;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 import java.time.Instant;
 @Value
+@Jacksonized
+@Builder
 public class UpdateReview {
     Long userId;
     String description;
